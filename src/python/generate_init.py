822356624fb78bbb6dadf8ac9ae83fd04bf4848e
Owen Jow <owenjow@berkeley.edu>
2020-03-15 04:08:01 -0700

    outer star

--- src/python/generate_init.py
+++ src/python/generate_init.py
@@ -77,6 +77,71 @@ def s_star(w, h):
 
     write_field('init_s_star.npy', s)
 
+def s_outerstar(w, h):
+    center = np.array([w // 2.0, h // 2.0])
+    binormal = np.array([0.0, 0.0, 1.0])
+    s = np.zeros((h, w))
+
+    # outer star points
+    # clockwise order starting from top point
+    # rotate 72 degrees around center each time
+    point_radius = 7.0 * h / 16.0
+    x0 = center + np.array([0, -point_radius])
+    x2 = center + np.array([ cos(18), -sin(18)]) * point_radius
+    x4 = center + np.array([ cos(54),  sin(54)]) * point_radius
+    x6 = center + np.array([-cos(54),  sin(54)]) * point_radius
+    x8 = center + np.array([-cos(18), -sin(18)]) * point_radius
+
+    def intersection(o1, d1, o2, d2):
+        """Compute the intersection of rays,
+        assuming that the rays DO intersect."""
+        o_diff = o2 - o1
+        denom = d1[0] * d2[1] - d2[0] * d1[1]
+        assert denom != 0
+        t1 = (d2[1] * o_diff[0] - d2[0] * o_diff[1]) / denom
+        return o1 + t1 * d1
+
+    # inner star points
+    x1 = intersection(x0, x4 - x0, x8, x2 - x8)
+    x3 = intersection(x2, x6 - x2, x0, x4 - x0)
+    x5 = intersection(x4, x8 - x4, x2, x6 - x2)
+    x7 = intersection(x6, x0 - x6, x4, x8 - x4)
+    x9 = intersection(x8, x2 - x8, x6, x0 - x6)
+
+    # joining lines
+    def fill_segment(start, end, width=30):
+        # compute direction
+        direction = end - start
+        dist = np.linalg.norm(direction)
+        direction /= dist
+
+        # compute normal
+        direction3d = np.pad(direction, (0, 1), 'constant')
+        normal = np.cross(binormal, direction3d)[:2]
+        normal /= np.linalg.norm(normal)
+
+        # fill flow field for segment
+        for t in np.arange(0, dist, 0.25):
+            x = start + direction * t
+            for u in np.arange(-width / 2, width / 2, 0.25):
+                xs = x + normal * u
+                xs = np.round(xs).astype(np.int)
+                if (xs >= 0).all() and xs[0] < w and xs[1] < h:
+                    s[xs[1], xs[0]] = 1.0
+
+    fill_segment(x0, x1)
+    fill_segment(x1, x2)
+    fill_segment(x2, x3)
+    fill_segment(x3, x4)
+    fill_segment(x4, x5)
+    fill_segment(x5, x6)
+    fill_segment(x6, x7)
+    fill_segment(x7, x8)
+    fill_segment(x8, x9)
+    fill_segment(x9, x0)
+
+    write_field('init_s_outerstar.npy', s)
+
 # ========
 # Velocity
 # ========
@@ -157,6 +222,71 @@ def v_star(w, h):
 
     write_field('init_v_star.npy', flow_field)
 
+def v_outerstar(w, h):
+    center = np.array([w // 2.0, h // 2.0])
+    binormal = np.array([0.0, 0.0, 1.0])
+    flow_field = np.zeros((h, w, 2))
+
+    # outer star points
+    # clockwise order starting from top point
+    # rotate 72 degrees around center each time
+    point_radius = 7.0 * h / 16.0
+    x0 = center + np.array([0, -point_radius])
+    x2 = center + np.array([ cos(18), -sin(18)]) * point_radius
+    x4 = center + np.array([ cos(54),  sin(54)]) * point_radius
+    x6 = center + np.array([-cos(54),  sin(54)]) * point_radius
+    x8 = center + np.array([-cos(18), -sin(18)]) * point_radius
+
+    def intersection(o1, d1, o2, d2):
+        """Compute the intersection of rays,
+        assuming that the rays DO intersect."""
+        o_diff = o2 - o1
+        denom = d1[0] * d2[1] - d2[0] * d1[1]
+        assert denom != 0
+        t1 = (d2[1] * o_diff[0] - d2[0] * o_diff[1]) / denom
+        return o1 + t1 * d1
+
+    # inner star points
+    x1 = intersection(x0, x4 - x0, x8, x2 - x8)
+    x3 = intersection(x2, x6 - x2, x0, x4 - x0)
+    x5 = intersection(x4, x8 - x4, x2, x6 - x2)
+    x7 = intersection(x6, x0 - x6, x4, x8 - x4)
+    x9 = intersection(x8, x2 - x8, x6, x0 - x6)
+
+    # joining lines
+    def fill_segment(start, end, width=30):
+        # compute direction
+        direction = end - start
+        dist = np.linalg.norm(direction)
+        direction /= dist
+
+        # compute normal
+        direction3d = np.pad(direction, (0, 1), 'constant')
+        normal = np.cross(binormal, direction3d)[:2]
+        normal /= np.linalg.norm(normal)
+
+        # fill flow field for segment
+        for t in np.arange(0, dist, 0.25):
+            x = start + direction * t
+            for u in np.arange(-width / 2, width / 2, 0.25):
+                xs = x + normal * u
+                xs = np.round(xs).astype(np.int)
+                if (xs >= 0).all() and xs[0] < w and xs[1] < h:
+                    flow_field[xs[1], xs[0]] = direction * 150.0
+
+    fill_segment(x0, x1)
+    fill_segment(x1, x2)
+    fill_segment(x2, x3)
+    fill_segment(x3, x4)
+    fill_segment(x4, x5)
+    fill_segment(x5, x6)
+    fill_segment(x6, x7)
+    fill_segment(x7, x8)
+    fill_segment(x8, x9)
+    fill_segment(x9, x0)
+
+    write_field('init_v_outerstar.npy', flow_field)
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('fn_name', type=str)
@@ -172,10 +302,12 @@ if __name__ == '__main__':
         # s
         s_fixed(w, h)
         s_star(w, h)
+        s_outerstar(w, h)
         # v
         v_circular(w, h)
         v_straight(w, h)
         v_constant(w, h)
         v_star(w, h)
+        v_outerstar(w, h)
     else:
         eval(args.fn_name)(w, h)
